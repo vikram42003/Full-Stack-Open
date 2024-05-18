c401b15379a2708507f33ac15363ba609120134f
vikram(wsl) <vikramjit360@gmail.com>
2024-05-18 16:15:02 +0530

    Finish part1 exercise 1.1 - courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
+    <div>
+      <Header course={course} />
+      <Content
+        courseNames={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
+    </div>
+  );
+};
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Content = ({ courseNames, exercises }) => {
+  return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        {courseNames[0]} {exercises[0]}
+      </p>
+      <p>
+        {courseNames[1]} {exercises[1]}
+      </p>
+      <p>
+        {courseNames[2]} {exercises[2]}
       </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+const Total = ({ exercises }) => {
+  return (
+    <p>Number of exercises {exercises[0] + exercises[1] + exercises[2]}</p>
+  );
+};
+
+export default App;
