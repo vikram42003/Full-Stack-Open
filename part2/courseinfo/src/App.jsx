3eb85e7f269fb69722a914f3abc73593951dec8f
vikram(wsl) <vikramjit360@gmail.com>
2024-06-20 00:02:05 +0530

    Finish part 2 exercise 2.2 - courseinfo step 7
    - add Total component

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -21,9 +21,9 @@ const Course = ({ course }) => {
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      {/* <Total
+      <Total
         sum={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
-      /> */}
+      />
     </div>
   );
 };
