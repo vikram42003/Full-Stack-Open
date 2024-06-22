b969fbc248250e87c5f50619ba48a90c2300344c
vikram(wsl) <vikramjit360@gmail.com>
2024-06-22 21:29:01 +0530

    Finish part 2 exercise 2.8 - phonebook step 3
    - add functionality to allow numbers to be added along with name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,29 +1,56 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    {
+      name: "Arto Hellas",
+      number: "040-1234567",
+    },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNewNameChange = (event) => {
     setNewName(event.target.value);
-  }
+  };
 
-  const handleAddName = (event) => {
+  const handleNewNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleAddPerson = (event) => {
     event.preventDefault();
-    if (persons.some(person => person.name === newName)) {
-      window.alert(`${newName} is already added to the phonebook`)
+
+    if (persons.some((person) => person.name === newName)) {
+      window.alert(`${newName} is already added to the phonebook`);
       return;
     }
-    setPersons((p) => [...p, { name: newName }]);
+
+    if (newNumber === "") {
+      window.alert("Number field cannot be empty");
+      return;
+    }
+
+    setPersons((p) => [...p, { name: newName, number: newNumber }]);
     setNewName("");
-  }
+    setNewNumber("");
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleAddName}>
+      <form onSubmit={handleAddPerson}>
         <div>
-          name: <input value={newName} onChange={handleNewNameChange}/>
+          name:{" "}
+          <input type="text" value={newName} onChange={handleNewNameChange} />
+        </div>
+        <div>
+          number:{" "}
+          <input
+            type="text"
+            value={newNumber}
+            onChange={handleNewNumberChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -32,7 +59,7 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {persons.map((person) => {
-          return <div key={person.name}>{person.name}</div>;
+          return <div key={person.name}>{person.name} {person.number}</div>;
         })}
       </div>
     </div>
