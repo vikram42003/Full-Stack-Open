414b3bcacdeb2865ac30cbeed954dfe090b04032
vikram(wsl) <vikramjit360@gmail.com>
2024-06-22 22:37:26 +0530

    Finish part 2 exercise 2.9 - phonebook step 4
    - add search functionality
    - make event handlers more compact
    - make minor modifications to webpage structure

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,21 +2,18 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: "Arto Hellas",
-      number: "040-1234567",
-    },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [query, setQuery] = useState("");
 
-  const handleNewNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNewNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
+  const personsToDisplay = persons.filter((person) =>
+    person.name.toLowerCase().includes(query.toLowerCase())
+  );
 
   const handleAddPerson = (event) => {
     event.preventDefault();
@@ -31,7 +28,7 @@ const App = () => {
       return;
     }
 
-    setPersons((p) => [...p, { name: newName, number: newNumber }]);
+    setPersons((p) => [...p, { name: newName, number: newNumber, id: persons.length + 1 }]);
     setNewName("");
     setNewNumber("");
   };
@@ -39,28 +36,52 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleAddPerson}>
-        <div>
-          name:{" "}
-          <input type="text" value={newName} onChange={handleNewNameChange} />
-        </div>
-        <div>
-          number:{" "}
-          <input
-            type="text"
-            value={newNumber}
-            onChange={handleNewNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
+
       <div>
-        {persons.map((person) => {
-          return <div key={person.name}>{person.name} {person.number}</div>;
-        })}
+        filter shown with{" "}
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <h2>Add a new</h2>
+        <form onSubmit={handleAddPerson}>
+          <div>
+            name:{" "}
+            <input
+              type="text"
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+            />
+          </div>
+          <div>
+            number:{" "}
+            <input
+              type="text"
+              value={newNumber}
+              onChange={(e) => setNewNumber(e.target.value)}
+            />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+      </div>
+
+      <div>
+        <h2>Numbers</h2>
+        <div>
+          {personsToDisplay.map((person) => {
+            return (
+              <div key={person.id}>
+                {person.name} {person.number}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
